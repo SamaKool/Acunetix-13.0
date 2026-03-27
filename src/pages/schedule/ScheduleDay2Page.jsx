bce5487834a8ca99a09a77db2b1e3c8c9776163e
SamaKool <kulkarnisrk01@gmail.com>
2026-03-28 00:14:39 +0530

    Updated 2nd Day Schedule

--- src/pages/schedule/ScheduleDay2Page.jsx
+++ src/pages/schedule/ScheduleDay2Page.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ScheduleDayPage from '@/pages/schedule/ScheduleDayPage';
 
 const scheduleEvents = [
-  { id: 1, name: 'DPL', location: 'LIBRARY', time: '9:30 AM' },
-  { id: 2, name: 'ESCAPE ROOM', location: '509 AND 511', time: '9:30 AM' },
-  { id: 3, name: 'BRAINIAC', location: '409', time: '9:30 AM' },
+  { id: 1, name: 'DPL', location: 'LIBRARY', time: '9 AM' },
+  { id: 2, name: 'ESCAPE ROOM', location: '509 AND 511', time: '9 AM' },
+  { id: 3, name: 'BRAINIAC', location: '402 AND 409', time: '9 AM' },
   { id: 4, name: 'CODE OF LIES', location: '5TH FLOOR LABS', time: '10 AM' },
 ];
 
